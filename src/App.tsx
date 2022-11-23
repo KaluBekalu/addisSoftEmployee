f45b0fdc526c9f128b9a79a98e08dfdf7b59c385
kalubekalu <kalubekalu1@gmail.com>
2022-11-23 12:55:10 +0300

    app cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
+    <div>
       <h1>Helloo AddisSoft</h1>
     </div>
   );
